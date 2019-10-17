d27a35a4cdbe91d7fae12cd03335677ddbb74781
sevenmaster <seven-master@hotmail.de>
2019-10-17 20:44:02 +0200

    check max work hours and department name

--- src/main/java/checker/Checker.java
+++ src/main/java/checker/Checker.java
@@ -16,14 +16,22 @@ public class Checker {
      * @return error message that may be outputted to the user. Or an empty String if no error was found
      */
     public String check(FullDocumentation toCheck) {
+        // TODO call functions and return error messages
         return "";
     }
-
     /**
      * Check if sum is correct and less or equal max working hours per month
      */
     private String checkSum(FullDocumentation toCheck) {
         TimeSpan sum = new TimeSpan(0, 0);
         Arrays.stream(toCheck.getEntries()).forEach(e -> sum.add(e.getWorkingTime()));
+        // worked more than max work hours
+        if (sum.compareTo(toCheck.getMaxWorkTime()) > 0)
+            return ErrorMessages.workedToMuch;
+        return ErrorMessages.none;
+    }
+
+    private String nameNotEmpty(FullDocumentation toCheck)  {
+        return toCheck.getDepartmentName().equals("") ? ErrorMessages.nameMissing : ErrorMessages.none;
     }
 }
---
+++ src/main/java/checker/ErrorMessages.java
@@ -0,0 +1,7 @@
+package checker;
+
+public class ErrorMessages {
+    public static final String none = "";
+    public static final String workedToMuch = "Zu viel gearbeitet.";
+    public static final String nameMissing = "Kein Institutsname angegeben";
+}
