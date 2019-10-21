595ed2048f9166096a682fb365069e5d5f1bc1c0
sevenmaster <seven-master@hotmail.de>
2019-10-21 12:14:01 +0200

    updated wage in example config to current wage

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -7,15 +7,15 @@ import org.json.*;
  * @author Liam Wachter
  */
 public class Parser implements IParser {
-    
-    String name;
-    String personnelNumber;
-    String institute;
-    int workingHours;
-    double wage;
-    boolean gf;
-    boolean ub;    
-    
+
+    private String name;
+    private String personnelNumber;
+    private String institute;
+    private int workingHours;
+    private double wage;
+    private boolean gf;
+    private boolean ub;
+
     @Override
     public FullDocumentation parse(String global, String month) throws IllegalArgumentException {
         
