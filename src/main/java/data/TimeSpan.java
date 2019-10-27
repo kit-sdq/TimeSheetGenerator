e8fcd21589fe46f15e309b0d4a9496a0ae12bf5f
Moritz Gstuer <moritz@gstuer.com>
2019-10-27 20:46:54 +0100

    Implemented substract and add methods to TimeSpan
    -Added substract method to TimeSpan
    -Added validity check to constructor of TimeSpan
    -Added a JUnit test class for arithmetic of TimeSpan
    -Added a JUnit test class for common functionality of TimeSpan
    -Added documentation to TimeSpan
    -Fixed add method not working properly

--- src/main/java/data/TimeSpan.java
+++ src/main/java/data/TimeSpan.java
@@ -9,33 +9,64 @@ public class TimeSpan implements Comparable {
     private int minute;
     private int hour;
 
+    /**
+     * Constructs a new TimeSpan instance.
+     * @param hour - Non-negative amount of hours
+     * @param minute - Number of minutes between 0 and 59
+     */
     public TimeSpan(int hour, int minute) {
-        // TODO check valid
+        if (hour < 0 || minute < 0) {
+            throw new IllegalArgumentException("Hour and minute may not be negative.");
+        } else if (minute > 59) {
+            throw new IllegalArgumentException("Minute may not be greater than 59.");
+        }
         this.minute = minute;
         this.hour = hour;
     }
 
+    /**
+     * Gets the minute of the TimeSpan.
+     * @return - The minute
+     */
     public int getMinute() {
         return minute;
     }
 
+    /**
+     * Gets the hour of the TimeSpan.
+     * @return - The hour
+     */
     public int getHour() {
         return hour;
     }
 
     /**
-     * add toAdd to this time span in the sense of time arithmetic. e.g 0:59 + 0:01  = 1:0
+     * Sums up hours and minutes taking carryover into account.
+     * @param addend - TimeSpan that should be added
      */
-    public void add(TimeSpan toAdd) {
-        minute += toAdd.getMinute();
-        minute %= 60;
-        hour += toAdd.getMinute() / 60;
-        hour += toAdd.getHour();
+    public void add(TimeSpan addend) {
+        int hourSum = this.hour + addend.getHour();
+        int minuteSum = this.minute + addend.getMinute();
+        int carry = minuteSum / 60;
+        
+        this.hour = hourSum + carry;
+        this.minute = minuteSum % 60;
     }
 
-    public void subtract(TimeSpan toSub) throws IllegalArgumentException {
-        //TODO subtract
-        //TODO check result if negative throw exception
+    /**
+     * Subtracts hours and minutes taking carryover into account.
+     * @param addend - TimeSpan that should be added
+     */
+    public void subtract(TimeSpan subtrahend) throws IllegalArgumentException {
+        if (this.compareTo(subtrahend) < 0) {
+            throw new IllegalArgumentException("Subtrahend may not be greater than Minuend.");
+        }
+        
+        int hourDiff = this.hour - subtrahend.getHour();
+        int minuteDiff = this.minute - subtrahend.getMinute();
+        
+        this.hour = hourDiff - ((59 - minuteDiff) / 60);
+        this.minute = (60 + minuteDiff) % 60;
     }
 
     @Override
@@ -46,10 +77,12 @@ public class TimeSpan implements Comparable {
     @Override
     public int compareTo(Object o) {
         TimeSpan timeSpan = (TimeSpan) o;
-        if (hour > timeSpan.getHour())
+        if (hour > timeSpan.getHour()) {
             return 1;
-        if (hour == timeSpan.getHour())
+        } else if (hour == timeSpan.getHour()) {
             return Integer.compare(minute, timeSpan.getMinute());
-        return -1;
+        } else {
+            return -1;
+        }
     }
 }
---
+++ src/test/java/data/TimeSpanCommonTest.java
@@ -0,0 +1,100 @@
+package data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimeSpanCommonTest {
+
+    @Test
+    public void testConstructor1() {
+        TimeSpan ts = new TimeSpan(22, 17);
+        
+        assertEquals(ts.getHour(), 22);
+        assertEquals(ts.getMinute(), 17);
+    }
+    
+    @Test
+    public void testConstructor2() {
+        TimeSpan ts = new TimeSpan(0, 0);
+        
+        assertEquals(ts.getHour(), 0);
+        assertEquals(ts.getMinute(), 0);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor3() {
+        new TimeSpan(-1, 3);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor4() {
+        new TimeSpan(1, -3);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor5() {
+        new TimeSpan(2, 60);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor6() {
+        new TimeSpan(0, 322);
+    }
+    
+    @Test
+    public void testToString1() {
+        TimeSpan ts = new TimeSpan(0, 0);
+        
+        assertEquals(ts.toString(), "00:00");
+    }
+    
+    @Test
+    public void testToString2() {
+        TimeSpan ts = new TimeSpan(2, 0);
+        
+        assertEquals(ts.toString(), "02:00");
+    }
+    
+    @Test
+    public void testToString3() {
+        TimeSpan ts = new TimeSpan(0, 4);
+        
+        assertEquals(ts.toString(), "00:04");
+    }
+    
+    @Test
+    public void testToString4() {
+        TimeSpan ts = new TimeSpan(20, 9);
+        
+        assertEquals(ts.toString(), "20:09");
+    }
+    
+    @Test
+    public void testToString5() {
+        TimeSpan ts = new TimeSpan(7, 36);
+        
+        assertEquals(ts.toString(), "07:36");
+    }
+    
+    @Test
+    public void testToString6() {
+        TimeSpan ts = new TimeSpan(56, 13);
+        
+        assertEquals(ts.toString(), "56:13");
+    }
+    
+    @Test
+    public void testToString7() {
+        TimeSpan ts = new TimeSpan(102, 7);
+        
+        assertEquals(ts.toString(), "102:07");
+    }
+    
+    @Test
+    public void testToString8() {
+        TimeSpan ts = new TimeSpan(1923, 0);
+        
+        assertEquals(ts.toString(), "1923:00");
+    }
+}
