8ca219319eb291feb8dad3e6faf1a1ead7503b51
Lars <57503246+larsk21@users.noreply.github.com>
2019-12-01 18:56:19 +0100

    Add getErrors to IChecker
    already implemented in MiLoGChecker

--- src/main/java/checker/IChecker.java
+++ src/main/java/checker/IChecker.java
@@ -1,7 +1,11 @@
 package checker;
 
+import java.util.Collection;
+
 public interface IChecker {
 
     public CheckerReturn check() throws CheckerException;
+    
+    public Collection<CheckerError> getErrors();
 
 }
