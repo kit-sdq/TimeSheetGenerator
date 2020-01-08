b1ecac361d83be44d8a720c4fecc05a0a1af83fe
larsk21 <koeniglars@outlook.com>
2020-01-08 10:11:50 +0100

    Ignore $schema in JSON files
    (merge between schema introduction and Jackson JSON)

--- src/main/java/parser/json/GlobalJson.java
+++ src/main/java/parser/json/GlobalJson.java
@@ -1,11 +1,13 @@
 package parser.json;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import data.TimeSpan;
 import data.WorkingArea;
 
+@JsonIgnoreProperties({ "$schema" })
 class GlobalJson {
 
     private final String name;
--- src/main/java/parser/json/MonthJson.java
+++ src/main/java/parser/json/MonthJson.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import data.TimeSpan;
 
+@JsonIgnoreProperties({ "$schema" })
 class MonthJson {
     
     private final YearMonth yearMonth;
