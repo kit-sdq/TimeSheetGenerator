64be72f0260f5fecb2cb9d2b038c09fdb5d66af2
Thomas Weber <thomas.weber@student.kit.edu>
2020-04-04 12:55:38 +0200

    Added gitignore, readme, renamed files and changed dockerfile
    Using ubuntu instead of alpine, tinytex insteal of texlive

--- docker/docker_build_tex_pdf.sh
+++ docker/docker_build_tex_pdf.sh
@@ -1,12 +1,11 @@
-#!/bin/ash
+#!/bin/bash
 # Arguments $1 global.json $2 month.json
-Xvfb :99 -screen 0 1920x1080x16 &
-export DISPLAY=:99
 cd /prod/
 mkdir -p /prod/aux
 withoutExtension=${2%.*}
 filename=${withoutExtension##*/}
 java -jar TimeSheetGenerator.jar --file "$1" "$2" /prod/aux/${filename%.*}.tex
-cd /prod/src/
+cd /prod/aux/
+cp /prod/Latex_Logo.pdf /prod/aux/Latex_Logo.pdf
 pdflatex --output-directory=/prod/aux/ /prod/aux/${filename%.*}.tex
 cp /prod/aux/${filename%.*}.pdf /prod/src/${filename%.*}.pdf
