9c79630a3d40e502019d8342dd2739942e508924
larsk21 <57503246+larsk21@users.noreply.github.com>
2020-05-10 08:18:12 +0200

    Remove old Docker scripts

--- docker/user_build_pdf.cmd
+++
@@ -1,8 +0,0 @@
-docker build -t timesheetgenerator:latest .
-SET SRC_PATH=%cd%
-SET IMAGE_NAME=timesheetgenerator:latest
-SET GLOBAL_JSON=global.json
-SET MONTH_JSON=month.json
-REM docker pull %IMAGE_NAME%
-docker run --rm -it -v "%SRC_PATH%":/prod/src %IMAGE_NAME% /prod/docker_build_tex_pdf.sh /prod/src/%GLOBAL_JSON% /prod/src/%MONTH_JSON%
-PAUSE
--- docker/user_build_pdf_hub.cmd
+++
@@ -1,7 +0,0 @@
-SET SRC_PATH=%cd%
-SET IMAGE_NAME=thomasweber/timesheetgenerator:latest
-SET GLOBAL_JSON=global.json
-SET MONTH_JSON=month.json
-docker pull %IMAGE_NAME%
-docker run --rm -it -v "%SRC_PATH%":/prod/src %IMAGE_NAME% /prod/docker_build_tex_pdf.sh /prod/src/%GLOBAL_JSON% /prod/src/%MONTH_JSON%
-PAUSE
