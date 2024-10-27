bea1701dcecb3bf5542f9a88a672daf0b467023f
JustOneDeveloper | Benni <78445321+Just1Developer@users.noreply.github.com>
2024-10-27 19:09:01 +0100

    JSON

---
+++ UITimesheetGenerator/src/json/Global.java
@@ -0,0 +1,32 @@
+package json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Global {
+    private String name;
+    private int staffId;
+    private String department;
+    private String workingTime;
+    private double wage;
+    private String workingArea;
+
+    // Constructors, Getters, and Setters
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getStaffId() { return staffId; }
+    public void setStaffId(int staffId) { this.staffId = staffId; }
+
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
+
+    public String getWorkingTime() { return workingTime; }
+    public void setWorkingTime(String workingTime) { this.workingTime = workingTime; }
+
+    public double getWage() { return wage; }
+    public void setWage(double wage) { this.wage = wage; }
+
+    public String getWorkingArea() { return workingArea; }
+    public void setWorkingArea(String workingArea) { this.workingArea = workingArea; }
+}
---
+++ UITimesheetGenerator/src/json/Month.java
@@ -0,0 +1,69 @@
+package json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class Month {
+    private int year;
+    private int month;
+
+    @JsonProperty("pred_transfer")
+    private String predTransfer;
+
+    @JsonProperty("succ_transfer")
+    private String succTransfer;
+    private List<Entry> entries;
+
+    // Constructors, Getters, and Setters
+    public Month() {}
+
+    // Nested class for individual entries
+    public static class Entry {
+        private String action;
+        private int day;
+        private String start;
+        private String end;
+        private String pause;
+        private boolean vacation;
+
+        // Constructors, Getters, and Setters
+
+        public Entry() {}
+
+        public String getAction() { return action; }
+        public void setAction(String action) { this.action = action; }
+
+        public int getDay() { return day; }
+        public void setDay(int day) { this.day = day; }
+
+        public String getStart() { return start; }
+        public void setStart(String start) { this.start = start; }
+
+        public String getEnd() { return end; }
+        public void setEnd(String end) { this.end = end; }
+
+        public String getPause() { return pause; }
+        public void setPause(String pause) { this.pause = pause; }
+
+        public boolean isVacation() { return vacation; }
+        public void setVacation(boolean vacation) { this.vacation = vacation; }
+    }
+
+    // Getters and Setters for Month class fields
+
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+
+    public int getMonth() { return month; }
+    public void setMonth(int month) { this.month = month; }
+
+    public String getPredTransfer() { return predTransfer; }
+    public void setPredTransfer(String predTransfer) { this.predTransfer = predTransfer; }
+
+    public String getSuccTransfer() { return succTransfer; }
+    public void setSuccTransfer(String succTransfer) { this.succTransfer = succTransfer; }
+
+    public List<Entry> getEntries() { return entries; }
+    public void setEntries(List<Entry> entries) { this.entries = entries; }
+}
